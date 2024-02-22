78f2107b6a0cf144751f95dcac1f10f991d9fe90
Livio Simard <liviosimard@yahoo.fr>
2024-02-22 21:21:12 +0100

    add base for ChessTournamentSoftware project

---
+++ controllers/MainController.py
@@ -0,0 +1,27 @@
+from views.MainView import MainView
+
+
+class Main:
+
+    def start(self):
+        while True:
+            choice = MainView.UserChoice()
+            if choice == 1:
+                player = CreatePlayer().RetrievePlayer()
+                MainView.RecapPlayerRegistered(player)
+            elif choice == 2:
+                MainView.EndProgram()
+                break
+
+
+class CreatePlayer:
+
+    def RetrievePlayer(self):
+        return {
+            "Name": MainView.RetrieveName(),
+            "Surname": MainView.RetrieveSurname(),
+            "Birth Date": MainView.RetrieveBirthDate()
+        }
+
+
+Main().start()
